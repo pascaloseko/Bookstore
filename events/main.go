1dd88242bab21fa711cbfb2275b6062cff6f5014
Pascal <pascal.ondieki21@gmail.com>
2019-03-23 09:42:20 +0300

    main server

--- events/main.go
+++ events/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
-	
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/pascaloseko/Events/events/rest"
+
+	"github.com/pascaloseko/Events/lib/config"
+)
+
+func main() {
+	confPath := flag.String("conf", `../lib/config/config.json`, "flag to set the path to the configuration json file")
+	flag.Parse()
+	// extract config details
+	conf, _ := config.ExtractConfiguration(*confPath)
+	fmt.Println("Server running on "+conf.RestfulEndpoint)
+	log.Println(rest.ServeAPI(conf.RestfulEndpoint))
 }
