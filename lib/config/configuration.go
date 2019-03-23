6a4f360f6d9403d3d520d1022562a891e0fb6e2e
Pascal <pascal.ondieki21@gmail.com>
2019-03-23 09:42:02 +0300

    config details for the server

--- lib/config/configuration.go
+++ lib/config/configuration.go
@@ -1,3 +1,34 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 // configuration details for the server and database
+
+//RestfulEPDefault ...
+var RestfulEPDefault = "localhost:8181"
+
+//ServiceConfig ...
+type ServiceConfig struct {
+	RestfulEndpoint string `json:"restfulapi_endpoint"`
+}
+
+//ExtractConfiguration extracts configuration details
+func ExtractConfiguration(filename string) (ServiceConfig, error) {
+	conf := ServiceConfig{
+		RestfulEPDefault,
+	}
+
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("Configuration file not found. Continuing with default values.")
+		return conf, err
+	}
+
+	err = json.NewDecoder(file).Decode(&conf)
+	return conf, err
+}
