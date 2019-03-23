8c5b3bf6a721e211e5de64386fd60e30c2205457
Pascal <pascal.ondieki21@gmail.com>
2019-03-23 09:41:19 +0300

    handlers for the events service

--- events/rest/handlers..go
+++ events/rest/handlers..go
@@ -1,3 +1,30 @@
 package rest
 
-// API handlers/controllers
+import (
+	"net/http"
+)
+
+// FindEventHandler search for events
+func FindEventHandler(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "This feature is coming soon", http.StatusNotImplemented)
+}
+
+// AllEventHandler gets all available events
+func AllEventHandler(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "This feature is coming soon", http.StatusNotImplemented)
+}
+
+// NewEventHandler adds a new event
+func NewEventHandler(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "This feature is coming soon", http.StatusNotImplemented)
+}
+
+// UpdatEventHandler updates event instance
+func UpdatEventHandler(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "This feature is coming soon", http.StatusNotImplemented)
+}
+
+// DeletEventHandler deletes an event instance
+func DeletEventHandler(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "This feature is coming soon", http.StatusNotImplemented)
+}
