f316af8fce80822b990dfaf784b8f7773c082708
Pascal <pascal.ondieki21@gmail.com>
2019-03-24 12:22:53 +0300

    renamed data to events

---
+++ persistence/events.go
@@ -0,0 +1,47 @@
+package persistence
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// CRUD functions
+
+// Db conn to database
+var Db *sql.DB
+var err error
+
+func init() {
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	dbURI := fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s", username, dbName, password)
+	fmt.Println(dbURI)
+	Db, err = sql.Open("postgres", dbURI)
+
+	if err != nil {
+		fmt.Printf("cannot open database: %v", err)
+	}
+}
+
+// AddEvent adds an event
+func (e *Event) AddEvent() (err error) {
+	if e.LocationID >= 1 {
+		err = errors.New("Location found")
+	} else {
+		err = errors.New("Location not found")
+	}
+	statement := "INSERT INTO event (name, duration, start_date, end_date, loc_id) VALUES($1, $2, $3, $4, (SELECT id FROM location WHERE id = $5)) RETURNING id;"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(e.Name, e.Duration, e.StartDate, e.EndDate, e.LocationID).Scan(&e.ID)
+	return
+}
